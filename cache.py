0f6e3d14391872602369be510c51a4cb21bf19bb
Felix Betke <s6febetk@uni-bonn.de>
2022-01-01 00:52:55 +0100

    added comments

--- cache.py
+++ cache.py
@@ -1,8 +1,20 @@
 import math
 import random
 from time import time
+from word import Word
 
 class CacheLine:
+    """
+    A helper class representing a cache line in a cache.
+
+    It may be used if no additional data is needed to
+    implement the cache replacement policy. An example of
+    this is the random replacement policy.
+
+    If additional information, such as the last time of
+    access, is needed by the replacement policy, this
+    class should be extended accordingly.
+    """
     data: list
     tag: int
     line_size: int
@@ -13,28 +25,91 @@ class CacheLine:
         self.line_size = line_size
 
     def isInUse(self):
+        """
+        Returns true if the current cache line is
+        in use by checking if the tag is set.
+        """
         return self.tag is not None
 
     def checkTag(self, tag: int) -> bool:
+        """
+        Returns if the given tag matches this cache
+        line's tag.
+
+        Parameters:
+            tag (int) -- the tag to compare
+
+        Returns:
+            bool: True if the cache line's tag matches
+        """
         return self.isInUse() and self.tag == tag
 
     def setTag(self, tag: int) -> None:
+        """
+        Sets the cache line's tag.
+
+        Parameters:
+            tag (int) -- the new tag
+
+        Returns:
+            This function does not have a return value.
+        """
         self.tag = tag
 
     def clearData(self) -> None:
+        """
+        Removes all data of this cache line and clears
+        the tag.
+        """
         for i in range(self.line_size):
             self.data[i] = None
 
         self.setTag(None)
 
     def read(self, offset: int) -> int:
+        """
+        Reads from the cache line at index 'offset'.
+        Note that this function does NOT check for
+        any tags.
+
+        Parameters:
+            offset (int) -- the offset at which to read
+
+        Returns:
+            int: The data saved at 'offset'.
+            Returns 'None' if no data is present.
+        """
         return self.data[offset]
 
     def write(self, offset: int, data: int) -> None:
+        """
+        Writes data to the cache line at index 'offset'.
+
+        Parameters:
+            offset (int) -- the offset to which to write
+            data (int)   -- the data to write
+
+        Returns:
+            This function does not have a return value.
+        """
         if data is None: return
         self.data[offset] = data
 
     def flush(self, offset: int) -> None:
+        """
+        Flushes the data written at the index 'offset'.
+        This means that further reads to this index
+        return 'None'.
+
+        If no more data is saved in this cache line,
+        the tag will be cleared as well.
+
+        Parameters:
+            offset (int) -- the offset at which to flush
+
+        Returns:
+            This function does not have a return value.
+        """
         self.data[offset] = None
 
         # clear tag if no more data is stored in this line
@@ -45,6 +120,12 @@ class CacheLine:
         self.setTag(None)
 
 class Cache:
+    """
+    An abstract class implementing a cache. Using this class directly is not possible, as it
+    does not implement any cache replacement policies.
+
+    Instead, use any of the following classes: CacheRR, CacheLRU, CacheFIFO
+    """
 
     num_sets:int
     num_lines: int
@@ -56,12 +137,22 @@ class Cache:
         self.num_lines = num_lines
         self.line_size = line_size
 
-    def __parseAddr__(self, addr: int) -> tuple:
+    def __parseAddr__(self, addr: int) -> tuple[int, int, int]:
+        """
+        Parses the given address and returns a 3-tuple consisting of
+        tag, index, and offset to access the cache.
+
+        Parameters:
+            addr (int) -- the address to parse
+
+        Returns:
+            tuple[int, int, int]: tag, index, offset
+        """
         num_offset_bits = math.floor(math.log2(self.line_size))
         num_index_bits = math.floor(math.log2(self.num_sets))
-        num_tag_bits = 16 - num_offset_bits - num_index_bits
+        num_tag_bits = Word.WIDTH - num_offset_bits - num_index_bits
 
-        addr_bits = bin(addr)[2:].zfill(16)
+        addr_bits = bin(addr)[2:].zfill(Word.WIDTH)
         tag = int(addr_bits[:num_tag_bits], base=2)
         index = int(addr_bits[num_tag_bits:num_tag_bits + num_index_bits], base=2)
         offset = int(addr_bits[num_tag_bits + num_index_bits:], base=2)
@@ -69,9 +160,33 @@ class Cache:
         return tag, index, offset
 
     def __applyReplacementPolicy__(self, addr: int, data: int) -> None:
+        """
+        Applies the corresponding replacement policy by choosing a cache line that
+        is to be replaced.
+        When called, all cache lines of a cache set must be already in use.
+
+        Parameters:
+            addr (int) -- the full address of the new cache line's entries.
+                          Note that the offset does not matter.
+            data (int) -- the data to be saved in the cache line.
+
+        Returns:
+            This function does not have a return value.
+        """
         raise Exception("Cache Replacement Policy not implemented.")
 
-    def read(self, addr) -> int:
+    def read(self, addr: int) -> int:
+        """
+        Returns the data at address addr as an integer.
+        If no data is cached for this address, None is returned.
+
+        Parameters:
+            addr (int) -- the address from which to read
+
+        Returns:
+            int: The cached data.
+            Returns 'None' if 'addr' is not cached.
+        """
 
         tag, index, offset = self.__parseAddr__(addr)
 
@@ -82,10 +197,21 @@ class Cache:
         return None
 
     def write(self, addr: int, data: int) -> None:
+        """
+        Adds 'data' to the cache, indexed by 'addr'.
+        If required, the replacement policy is applied.
 
+        Parameters:
+            addr (int) -- the address to which to write
+            data (int) -- the data to cache
+
+        Returns:
+            This function does not have a return value.
+        """
         tag, index, offset = self.__parseAddr__(addr)
-        print('T', tag, "i", index, 'o', offset)
 
+        # check if all cache lines of the corresponding cache
+        # set are already in use.
         for i in range(self.num_lines):
             if not self.sets[index][i].isInUse():
                 self.sets[index][i].setTag(tag)
@@ -93,9 +219,19 @@ class Cache:
                 self.sets[index][i].write(offset, data)
                 return
 
+        # apply replacement policy of all cache lines are in use
         self.__applyReplacementPolicy__(addr, data)
 
     def flush(self, addr: int) -> None:
+        """
+        Removes the data indexed by 'addr' from the cache.
+
+        Parameters:
+            addr (int) -- the address to be flushed from the cache
+
+        Returns:
+            This function does not have a return value.
+        """
 
         tag, index, offset = self.__parseAddr__(addr)
 
@@ -105,6 +241,7 @@ class Cache:
                 return
 
     def printCache(self) -> None:
+        """Prints the cache. Only to be used during development."""
         for i in range(len(self.sets)):
             print(i, end=' ')
             for j in range(len(self.sets[i])):
@@ -117,6 +254,7 @@ class Cache:
             print('')
 
 class CacheRR(Cache):
+    """A cache implementing the random replacement policy."""
 
     def __init__(self, num_sets: int, num_lines: int, line_size: int):
         super().__init__(num_sets, num_lines, line_size)
@@ -131,7 +269,12 @@ class CacheRR(Cache):
 
 
 class CacheLineLRU(CacheLine):
+    """
+    A helper class representing a cache line in a cache that
+    implements the least-recently-used replacement policy.
+    """
 
+    # update this variable each time we read/write.
     lru_timestamp: float
 
     def __init__(self, line_size: int):
@@ -151,13 +294,24 @@ class CacheLineLRU(CacheLine):
     def getLRUTime(self):
         return self.lru_timestamp
 
+    # Should flushing count as an access to a cache line?
+    # If not, remove the following method:
+    def flush(self, offset: int) -> None:
+        super().flush(offset)
+        self.lru_timestamp = time()
+
 class CacheLRU(Cache):
+    """A cache implementing the least-recently-used replacement policy."""
 
     def __init__(self, num_sets: int, num_lines: int, line_size: int):
         super().__init__(num_sets, num_lines, line_size)
         self.sets = [[CacheLineLRU(line_size) for a in range(num_lines)] for b in range(num_sets)]
 
     def __applyReplacementPolicy__(self, addr: int, data: int):
+        """
+        Implements a least-recently-used policy by using the lru_timestamp variable
+        from CacheLineLRU.
+        """
 
         tag, index, offset = self.__parseAddr__(addr)
 
@@ -173,11 +327,17 @@ class CacheLRU(Cache):
         self.sets[index][lru_index].write(offset, data)
 
 class CacheLineFIFO(CacheLine):
+    """
+    A helper class representing a cache line in a cache that
+    implements the first-in-first-out replacement policy.
+    """
 
+    # update this variable on the first write/on initialization.
     first_write: float
 
     def __init__(self, line_size: int):
         super().__init__(line_size)
+        self.first_write = time()
 
     def write(self, offset: int, data: int) -> None:
         empty = True
@@ -195,12 +355,17 @@ class CacheLineFIFO(CacheLine):
         return self.first_write
 
 class CacheFIFO(Cache):
+    """A Cache implementing the first-in-first-out replacement policy."""
 
     def __init__(self, num_sets: int, num_lines: int, line_size: int):
         super().__init__(num_sets, num_lines, line_size)
         self.sets = [[CacheLineFIFO(line_size) for a in range(num_lines)] for b in range(num_sets)]
 
     def __applyReplacementPolicy__(self, addr: int, data: int):
+        """
+        Implements a least-recently-used policy by using the first_write variable
+        from CacheLineFIFO.
+        """
 
         tag, index, offset = self.__parseAddr__(addr)
 
@@ -215,7 +380,7 @@ class CacheFIFO(Cache):
         self.sets[index][fifo_index].setTag(tag)
         self.sets[index][fifo_index].write(offset, data)
 
-
+"""
 c = CacheFIFO(2, 2, 2)
 c.write(40, 40)
 c.write(0, 1)
@@ -223,3 +388,4 @@ c.printCache()
 c.write(100, 99)
 c.write(0, 1)
 c.printCache()
+"""
--- mmu.py
+++ mmu.py
@@ -1,23 +1,43 @@
-from cache import Cache
+from cache import CacheRR, CacheLRU, CacheFIFO
 from byte import Byte
 from word import Word
 
 class MMU:
+    """
+    The memory management unit (MMU).
+
+    In our model, the MMU includes the main memory and
+    cache. Contrary  to the Skylake architecture, our
+    MMU does not contain load- and store-buffers, and
+    for the sake of simplicity, there is one cache only.
+    Therefore, whether our cache is an L3 or L1 cache
+    does not matter.
+    """
 
     cache_hit_cycles: int = 2
     cache_miss_cycles: int = 5
 
     memory: list
     mem_size: int
-    cache: Cache
+    cache: CacheRR
 
     def __init__(self, mem_size: int):
         self.memory = [0] * mem_size
         self.mem_size = mem_size
-        self.cache = Cache(4, 4, 4)
+        self.cache = CacheRR(4, 4, 4)
 
     def read_byte(self, index: int) -> tuple[Byte, int]:
+        """
+        Reads one byte from memory and returns it along with
+        the number of cycles it takes to load it.
 
+        Parameters:
+            index (int) -- the memory address from which to read
+
+        Returns:
+            tuple[Byte, int]: Byte class that contains
+            data and the number of cycles needed to load it.
+        """
         data = self.cache.read(index)
         cycles = self.cache_hit_cycles
 
@@ -28,28 +48,61 @@ class MMU:
 
         return Byte(data), cycles
 
+
     def write_byte(self, index: int, data: Byte) -> None:
+        """
+        Writes a byte to memory.
+
+        Parameters:
+            index (int) -- the memory address to which to write
+            data (Byte) -- the Byte to write to this address
+
+        Returns:
+            This function does not have a return value.
+        """
 
         self.memory[index] = data.value
         self.cache.write(index, data.value)
 
     def read_word(self, index: int) -> tuple[Word, int]:
+        """
+        Reads one word from memory and returns it along with
+        the number of cycles it takes to load it.
+        The architecture is assumed to be little-endian.
+
+        Parameters:
+            index (int) -- the memory address from which to read
+
+        Returns:
+            tuple[Word, int]: Word class that contains the
+            data and the number of cycles needed to laod it.
+        """
 
         # little endian
         lower_half, cycles_lower = self.read_byte(index)
         upper_half, cycles_upper = self.read_byte(index + 1)
 
-        result = (upper_half.value << 8) + lower_half.value
+        result = (upper_half.value << (Word.WIDTH // 2)) + lower_half.value
 
         return result, max(cycles_lower, cycles_upper)
 
     def write_word(self, index: int, data: Word) -> None:
+        """
+        Writes a word to memory. The architecture is assumed to be little-endian.
+
+        Parameters:
+            index (int) -- the memory address to which to write
+            data (Word) -- the Word to write to this address
+
+        Returns:
+            This function does not have a return value.
+        """
 
         data = data.value
-        data_bits = bin(data)[2:].zfill(16)
+        data_bits = bin(data)[2:].zfill(Word.WIDTH)
 
-        lower_half = int(data_bits[8:], base=2)
-        upper_half = int(data_bits[:8], base=2)
+        lower_half = int(data_bits[Word.WIDTH // 2:], base=2)
+        upper_half = int(data_bits[:Word.WIDTH // 2], base=2)
 
         self.write_byte(index, Byte(lower_half))
         self.write_byte(index + 1, Byte(upper_half))
