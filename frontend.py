10c29551f523832b1f6d0143372e3481de1ad439
Melina Hoffmann <m.hoffmann@uni-bonn.com>
2022-01-05 18:55:32 +0100

    #7 Implementation of frontend started. Todo: test if this works and document

--- frontend.py
+++ frontend.py
@@ -1,42 +1,100 @@
-class InstructionQueue:
+import bpu
+import parser
+from collections import deque
+
+
+
+class Frontend:
 
     max_length : int #maximal number of instructions in the queue, can be ignored by µ-programs which are inserted b.c. an exception occured 
-    current_length : int #current number of instructions in the queue
-    pc: int #program counter ; ist there a "master pc" elsewhere? register? #dos this point to the start of the queue, the end of the queue, or the next instruction we fetch from the list?
+    pc: int #program counter ; points to the next instruction to fetch from the Instruction List into the Instruction Queue
+    bpu : BPU #branch prediction unit; should be set as a shallow copy by the CPU class
+    instr_list #List of instructions provided by the parser, should be set as a shallow copy by the CPU class; explicit type hint?
+    instr_queue #queue of the next max_length or less instructions that should be placed into the RS (more instr. possible if a µ-programm is executed)
+
 
     #initialise
     #length of queue
     #instruction pointer (where is the "master instruction pointer"? register?) -> maybe adjust during initialisation for weird edge or debugging cases
     #default maximum for length of the instruction queue was set arbitrarily to 5, open for discussion
-    def __init__(self, maximum=5) -> None:
+    #cpu_bpu and cpu_instr_list expect shallow copies of the bpu initialised by the cpu and the instruction list returned to the cpu by the parser respectively
+    def __init__(self, cpu_bpu, cpu_instr_list, maximum=5) -> None:
         self.max_length = maximum
         self.current_length = 0
         self.pc = 0
+        self.bpu = cpu_bpu
+        self.instr_list = cpu_instr_list
+        self.instr_queue = deque()
 
-    #get instruction from parser
-    #make sure that the length requirement is not violated
-    #recognise branches, calls etc.
-    #communicate with BPU and fill queue/ adjust pc accordingly
-    #recognise misprediction (communicate with with RS?), flush Queue from appropriate instruction on and fill again
-    #how does this work, if mispredicted instructions are already in the RS? Rollback to branch instruction and take different branch
-    #"remember" branching decisions
-    #update BPU after each branching instruction -> need signaling for all branching instructions, so we can also inform the BPU about succesfull predictions
-    def AddInstructionToQueue () -> None:
-        pass
+    def add_instruction_to_queue (self) -> None:
+
+        #check if next, instr is valid, i.e. pc < len(instr_list)
+        if self.pc > len(self.instr_list):
+            #do we have a functionality to properly raise errors?
+            #do we have something like an exit funtion/ operation or is this the only way a program ends?
+            print("end of program reached\n **goodbye**\n")
+            return
+
+        #make sure that the length requirement is not violated
+        if len(self.instr_queue) >= self.max_length:
+            return
+            
+        current_instr : Instruction = self.instr_list[self.pc]
+
+        #append instr to queue
+        self.instr_queue.append(current_instr)
+
+        #this needs to be modified if further jump instruction types are implemented
+        #does comparing for a class work like this?
+        if current_instr.ty == InstrBranch:
+        
+            #get branch prediction from bpu
+            #true if branch was/ should be taken (?)
+            prediction : bool = self.bpu.predict(self.pc)
+
+            if prediction == True:
+                self.pc = current_instr.ops[0]
+
+            else:
+                self.pc = self.pc + 1
+
+            #no further handling of the bpu in this step
+            #here: direct contact with bpu, b.c. interface clear
+            #might need adjustment if we have a branch order buffer
+
+        else:            
+            #use setter function instead?
+            self.pc = self.pc + 1
+
 
     #hand over instruction to RS
-    #read/ fetch and delete first instruction
-    #will the instruction queue initialise this or the RS if it is free?
-    #in the latter case, would the RS fetch instructions from the Queue out of order, would it sit empty, or do we only have one RS for all instruction types anyway?
-    def FetchInstruction () -> None:
-        pass
+    #read/ fetch and delete first instruction from the queue
+    def fetch_instruction_from_queue (self) -> Instruction:
+
+        if len(self.instr_queue>0):
+            return self.instr_queue.popleft()
+
+        else:
+            print("no instruction in queue")
+            #TODO: proper handling/ interface design if the queue is empty
+            return None
 
     #flush Queue
-    #adjust instruction pointer?
-    def FlushQueue () -> None:
-        pass
+    #adjust instruction pointer so the flushed instructuions are "back in the queue"? 
+    # might not be feasible without further buffering for branches 
+    # -> maybe leave for those who initiate the flushing
+    def flush_instruction_queue (self) -> None:
+        self.instr_queue.clear()
 
     #add µ-program
-    #optionally flush queue first
-    def AddMicroProgram () -> None:
-        pass
+    #optionally flush queue first -> leave as mitigation
+    def add_micro_program (self, micro_prog : list[Instruction]) -> None:
+        
+        for current_instr in micro_prog:
+
+            self.instr_queue.append(current_instr)
+
+
+    #setter function for the program counter, e.g. for external rollback handling
+    #e.g. with property decorator?
+    #is this really necessary?
