a4e660779510ad90a23c94dae6ef0dcf45775895
LevitatingLion <levitatinglion@web.de>
2022-01-18 04:44:33 +0100

    #14 Partially fix unit tests (they need a rework anyways)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,37 +1,48 @@
-import unittest
 import logging
 import sys
-from src.parser import InstructionType, Instruction, Parser
+import unittest
+
+from src.instructions import Instruction, InstructionType, addi
+from src.parser import Parser
 
 logger = logging.getLogger()
 logger.level = logging.DEBUG
 stream_handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(stream_handler)
 
-class parserTests(unittest.TestCase):
 
+class parserTests(unittest.TestCase):
     def test_parser(self):
 
-        addi = InstructionType("addi", ["reg", "reg", "imm"])
-        j = InstructionType("j", ["label"])
+        j = InstructionType("j", None)
+        j.operand_types = ["label"]
         p = Parser()
 
         p.add_instruction(addi)
         p.add_instruction(j)
-        instrs = p.parse('''a:
+        instrs = p.parse(
+            """a:
                 addi r1, r0, 100
                 j a
-                ''')
+                """
+        )
 
-        self.assertTrue(instrs == [Instruction(addi, [1, 0, 100]),Instruction(j, [0]),])
+        self.assertTrue(
+            instrs
+            == [
+                Instruction(addi, [1, 0, 100]),
+                Instruction(j, [0]),
+            ]
+        )
 
         with self.assertRaises(Exception) as context:
             p.parse("invalid r0, 0")
-        self.assertTrue('Unknown instruction type' in str(context.exception))
+        self.assertTrue("Unknown instruction type" in str(context.exception))
 
         with self.assertRaises(Exception) as context:
             p.parse("addi r0, 0")
-        self.assertTrue('Wrong' in str(context.exception))
+        self.assertTrue("Wrong" in str(context.exception))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
