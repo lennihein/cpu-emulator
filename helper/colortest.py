a49076322d282dad056234ab87996ca17f64d80f
Lenni Hein <git@lennihein.com>
2022-02-17 23:53:39 +0100

    removed helper files

--- helper/colortest.py
+++
@@ -1,8 +0,0 @@
-x = 0
-for i in range(24):
-  colors = ""
-  for j in range(5):
-    code = str(x+j)
-    colors = colors + "\33[" + code + "m\\33[" + code + "m\033[0m "
-  print(colors)
-  x = x + 5
