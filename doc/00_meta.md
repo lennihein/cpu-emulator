3555b52009b76b3ee6e789d285b5b897d098c072
LevitatingLion <levitatinglion@web.de>
2022-02-28 17:11:13 +0100

    Add doc/ with pandoc template

---
+++ doc/00_meta.md
@@ -0,0 +1,29 @@
+---
+title: "TODO"
+subtitle: "TODO"
+author: "TODO"
+date: "TODO"
+lang: "en"
+thesis-type: "bachelor"
+thesis-type-custom: ""
+thesis-matrikel: ""
+thesis-degree-course: "Computer Science (M.Sc.)"
+thesis-submission: "TODO"
+thesis-supervisor-one: "TODO"
+thesis-supervisor-two:
+    name: "TODO"
+    affiliation: "TODO"
+thesis-sponsor: "TODO"
+thesis-affiliation: |
+    Rheinische Friedrich-Wilhelms-Universität Bonn \
+    Institut für Informatik IV \
+    Arbeitsgruppe für IT-Sicherheit \
+thesis-twosided: false
+thesis-monochrome: false
+thesis-table-position: "end"
+thesis-print-declaration: true
+thesis-reference-style: ""
+abstract: |
+    TODO
+thanks: ""
+---
---
+++ doc/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+set -u
+
+# all *.md files
+SOURCES=(*.md)
+# output file for thesis
+OUT="lab.pdf"
+# directory to build in
+BUILD="build"
+
+# options for pandoc
+FLAGS_PANDOC=(--template tpl/pandoc_template.tex \
+    --listings --top-level-division chapter --columns 1000 \
+    --filter pandoc-crossref --metadata autoSectionLabels \
+    --metadata listings --metadata codeBlockCaptions \
+    --bibliography bibliography.bib --biblatex)
+# options for latexmk
+FLAGS_LATEXMK=(-silent -Werror -outdir="$BUILD" -lualatex -halt-on-error)
+
+# location of tex file in build/
+BUILD_TEX="$BUILD/main.tex"
+# location of pdf file in build/
+BUILD_PDF="${BUILD_TEX/%.tex/.pdf}"
+
+main () {
+    # build tex from markdown using pandoc
+    mkdir -p "$BUILD"
+    run_log pandoc "${FLAGS_PANDOC[@]}" -o "$BUILD_TEX" "${SOURCES[@]}"
+
+    # center tables and restore default spacing
+    run_log sed -i 's/\\begin{longtable}\[\]{@{}\([^}]*\)@{}}/\\begin{longtable}\[c\]{\1}/g' "$BUILD_TEX"
+
+    # float tables
+    #run_log sed -i 's/\\begin{longtable}/\\begin{table}\[tbp\]\\begin{longtable}/g' "$BUILD_TEX"
+    #run_log sed -i 's/\\end{longtable}/\\end{longtable}\\end{table}/g' "$BUILD_TEX"
+
+    # build pdf from tex using latexmk
+    run_log latexmk "${FLAGS_LATEXMK[@]}" "$BUILD_TEX"
+ 
+    # copy pdf to output
+    run_log cp "$BUILD_PDF" "$OUT"
+}
+
+run_log () {
+    # print command and execute it
+
+    echo "$*" >&2
+    "$@" || exit
+}
+
+main "$@"
