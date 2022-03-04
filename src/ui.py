9e9d16e846f6f16bb2a196fa5200a6ae2df1339e
Lenni Hein <git@lennihein.com>
2022-03-04 15:02:22 +0100

    #6 #35 #21 #22: some refactoring required for side by side display. Also some beauty fixes

--- src/ui.py
+++ src/ui.py
@@ -21,6 +21,12 @@ UNDERLINE = '\033[4m'
 FAINT = '\33[2m'
 YELLOW = '\33[33m'
 
+BOX_SOUTHEAST = '╭'
+BOX_SOUTHWEST = '╮'
+BOX_NORTHEAST = '╰'
+BOX_NORTHWEST = '╯'
+BOX_HORIZOZTAL = '─'
+BOX_VERTICAL = '│'
 
 # get terminal size
 try:
@@ -31,7 +37,11 @@ except OSError:
 
 # print colored text using ANSI escape sequences
 def print_color(c, str, newline=False):
-    print(c + str + ENDC, end="\n" if newline else "")
+    print(fmt_color(c, str, newline=newline), end="")
+
+
+def fmt_color(c, str, newline=False):
+    return c + str + ENDC + ("\n" if newline else "")
 
 
 # print a simple divider
@@ -54,13 +64,20 @@ def print_header(str, c=ENDC):
 
 def print_hex(num: int, p_end=" ", base=True,
               base_style=FAINT, style=ENDC) -> None:
-    num_str = style + '{:04x}'.format(num) + ENDC
+    print(hex_str(num, p_end=p_end, base=base, base_style=base_style, style=style), end="")
+
+
+def hex_str(num: int, p_end=" ", base=True, fixed_width=True, base_style=FAINT, style=ENDC) -> str:
+    if fixed_width:
+        num_str = style + '{:04x}'.format(num) + ENDC
+    else:
+        num_str = style + '{:x}'.format(num) + ENDC
     base_str = base_style + "0x" + ENDC if base else ""
-    print(base_str + num_str, end=p_end)
+    return base_str + num_str + p_end
 
 
 def print_memory(mmu: MMU, lines=8, base=0x0000):
-    fits = (columns - 8) // 7
+    fits = (columns - 8) // 5
     i = base
     for _ in range(lines):
         if i >= mmu.mem_size:
@@ -70,9 +87,9 @@ def print_memory(mmu: MMU, lines=8, base=0x0000):
             if i >= mmu.mem_size:
                 return
             if(mmu.is_addr_cached(Word(i))):
-                print_hex(mmu.memory[i + 1] * 256 + mmu.memory[i], base_style=FAINT + RED, style=RED)
+                print_hex(mmu.memory[i + 1] * 256 + mmu.memory[i], base_style=FAINT + RED, style=RED, base=False)
             else:
-                print_hex(mmu.memory[i + 1] * 256 + mmu.memory[i])
+                print_hex(mmu.memory[i + 1] * 256 + mmu.memory[i], base=False)
             i += 2
         print()
 
@@ -115,64 +132,65 @@ def print_cache(mmu: MMU) -> None:
         print('')
 
 
-def print_instruction(instr: Instruction) -> int:
+def instruction_str(instr: Instruction) -> str:
     instr_str = YELLOW + instr.ty.name + ENDC
     instr_str += " " * (6 - len(instr.ty.name))
-    op_str = ", ".join([str(op) for op in instr.ops])
-    print(instr_str + op_str, end="")
-    return len(instr_str) + len(op_str)
+    op_str = ", ".join([hex_str((Word(op).value), p_end="", fixed_width=False) for op in instr.ops])
+    return instr_str + op_str
 
 
 def print_queue(queue: Frontend):
     for item in queue.instr_queue:
         instr = item.instr
-        print_instruction(instr)
-        print()
+        print(instruction_str(instr))
 
 
 def print_prog(front: Frontend, engine: ExecutionEngine,
                breakpoints: dict, start=0, end=-1):
+    prog = prog_str(front, engine, breakpoints, start, end)
+    for line in prog:
+        print(line)
 
+
+def prog_str(front: Frontend, engine: ExecutionEngine, breakpoints: dict, start=0, end=-1) -> list[str]:
     start = 0 if start < 0 else start
     end = len(front.instr_list) if end == -1 else end
     end = min(end, len(front.instr_list))
+    prog_str: list[str] = [""] * (end - start)
 
     inflights = [slot.pc for slot in engine.slots() if slot is not None]
     active_breakpoints = [pt for pt in breakpoints if breakpoints[pt]]
     disabled_breakpoints = [pt for pt in breakpoints if not breakpoints[pt]]
 
-    for i in range(start, end):
-
+    for index, line in enumerate(range(start, end)):
         # print status tag
-        if i in inflights and i in active_breakpoints:
-            print_color(BOLD + RED, "► ", False)
-        elif i in inflights:
-            print_color(BOLD + GREEN, "► ", False)
-        elif i in active_breakpoints:
-            print_color(BOLD + RED, "◉ ", False)
-        elif i in disabled_breakpoints:
-            print_color(BOLD + RED, "○ ", False)
+        if line in inflights and line in active_breakpoints:
+            prog_str[index] += fmt_color(BOLD + RED, "► ", False)
+        elif line in inflights:
+            prog_str[index] += fmt_color(BOLD + GREEN, "► ", False)
+        elif line in active_breakpoints:
+            prog_str[index] += fmt_color(BOLD + RED, "◉ ", False)
+        elif line in disabled_breakpoints:
+            prog_str[index] += fmt_color(BOLD + RED, "○ ", False)
         else:
-            print("  ", end="")
+            prog_str[index] += "  "
 
         # print line tag
-        line_str = str(i)
+        line_str = str(line)
         line_str = " " * (2 - len(line_str)) + line_str + " "
-        print(FAINT + line_str + ENDC, end="")
+        prog_str[index] += FAINT + line_str + ENDC
 
         try:
             # print instruction
-            instr = front.instr_list[i]
-            print_instruction(instr)
+            instr = front.instr_list[line]
+            prog_str[index] += instruction_str(instr)
         except IndexError:
             print("\n")
-            print(f"i: {i}")
+            print(f"i: {line}")
             print(f"len: {len(front.instr_list)}")
             print(f"start: {start}")
             print(f"end: {end}")
-
-        # newline
-        print()
+    return prog_str
 
 
 def print_rs(cpu: CPU):
@@ -184,7 +202,9 @@ def print_rs(cpu: CPU):
         id_str = str(id)
         id_str = " " * (2 - len(id_str)) + id_str + " "
         print(FAINT + id_str + ENDC, end="")
-        i_len = print_instruction(slot.instr)
+        instr_str = instruction_str(slot.instr)
+        i_len = len(instr_str)
+        print(instr_str, end="")
         print(" " * (24 - i_len), end="")
         print(f"{' RUNNING' if slot.executing else 'RETIRING'}")
         id += 1
@@ -217,7 +237,7 @@ def header_prog(front: Frontend, engine: ExecutionEngine, breakpoints: dict):
     print()
     lowest_inflight = min([slot.pc for slot in engine.slots() if slot is not None], default=0)
     highest_inflight = max([slot.pc for slot in engine.slots() if slot is not None], default=len(front.instr_list))
-    print_prog(front, engine, breakpoints, start=lowest_inflight - 2, end=highest_inflight + 2)
+    print_prog(front, engine, breakpoints, start=lowest_inflight - 1, end=highest_inflight + 2)
     print()
 
 
